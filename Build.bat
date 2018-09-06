410a8fe24d54d841dad5cd30a6204780f16fb550
Somnath Mukherjee <somnathmukherjee81@gmail.com>
2018-09-06 15:55:13 -0600

    ci(project-manager-api): updating Build.bat

--- Build.bat
+++ Build.bat
@@ -2,7 +2,12 @@ dotnet restore
 dotnet build
 dotnet test
 dotnet publish ./src/Fsd.ProjectManager.Api/Fsd.ProjectManager.Api.csproj -c Release
-docker rmi -f project-manager-api
-docker build -t project-manager-api ./src/Fsd.ProjectManager.Api/
-docker images
+
+REM Uncomment to run on docker
+REM docker rmi -f project-manager-api
+REM docker build -t project-manager-api ./src/Fsd.ProjectManager.Api/
+REM docker images
 REM docker run -p 9090:9090 project-manager-api
+
+REM Uncomment to run with dotnet standalone
+dotnet ./src/Fsd.ProjectManager.Api/bin/release/netcoreapp2.0/publish/Fsd.ProjectManager.Api.dll
